05816a918c4941d2f0a6101fd3e8d3eeb9a45876
Erik Svensson <erik.svensson@regionorebrolan.se>
2026-02-03 23:30:22 +0100

    add status to model

--- client/frontend/src/app/shared/models/info.ts
+++ client/frontend/src/app/shared/models/info.ts
@@ -3,12 +3,14 @@ export class Info {
     title: string;
     text: string;
     publishDate: Date;
-    unpublished: Date | undefined;
-    constructor(id: string, title: string, text: string, publishDate: Date, unpublished: Date | undefined){
+    unpublished?: Date | undefined;
+    status?: string;
+    constructor(id: string, title: string, text: string, publishDate: Date, unpublished: Date | undefined, status: string){
         this.id = id;
         this.title = title;
         this.text = text;
         this.publishDate = publishDate;
         this.unpublished = unpublished;
+        this.status = status;
     }
 }
