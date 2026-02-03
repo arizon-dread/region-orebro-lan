11d45762de629ae4fcacf7303e9d8e6f0ef512a2
Erik Svensson <erik.svensson@regionorebrolan.se>
2026-02-04 00:54:23 +0100

    nullcheck info-input in modal

--- client/frontend/src/app/components/information/add-information/add-information.ts
+++ client/frontend/src/app/components/information/add-information/add-information.ts
@@ -5,10 +5,10 @@ import { Info } from '../../../shared/models/info';
 import { take } from 'rxjs';
 import { MatDialogContent, MatDialogModule, MatDialogActions, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { HttpErrorResponse } from '@angular/common/http';
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 @Component({
   selector: 'app-add-information',
-  imports: [ReactiveFormsModule, MatDialogContent, MatDialogModule, MatDialogActions, ],
+  imports: [ReactiveFormsModule, MatDialogContent, MatDialogModule, MatDialogActions,],
   templateUrl: './add-information.html',
   styleUrl: './add-information.css',
 })
@@ -20,30 +20,41 @@ export class AddInformation implements OnInit {
   });
 
 
-  constructor(private httpSvc: HttpService, @Inject(MAT_DIALOG_DATA) private data: Info) { }
+  constructor(private httpSvc: HttpService, @Inject(MAT_DIALOG_DATA) private data: Info | undefined) { }
   ngOnInit() {
-    this.form.patchValue({
-      title: this.data.title,
-      text: this.data.text,
-    });
-    this.form.updateValueAndValidity();
+    if (this.data) {
+      this.form.patchValue({
+        title: this.data.title,
+        text: this.data.text,
+      });
+      this.form.updateValueAndValidity();
+    }
   }
   reset() {
     this.form.reset();
     this.form.markAsUntouched();
     this.form.markAsPristine();
   }
-  submit() : Info | undefined {
+  submit(): Info | undefined {
     //validate form
+    let id: string;
+    let version: number;
+    if (!this.data) {
+      id = crypto.randomUUID();
+      version = 1;
+    } else {
+      id = this.data.id;
+      version = this.data.version;
+    }
     if (this.form.valid) {
       let info: Info = {
-        id: this.data.id,
+        id: id,
         title: this.form.controls.title.value,
         text: this.form.controls.text.value,
-        version: this.data.version,
+        version: version,
       }
       return info;
-      
+
     }
     return undefined;
   }
