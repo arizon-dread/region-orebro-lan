2c5611ff2606e3bb5e1955dcb35390252950959e
Tobbe Eriksson <torbjorn.eriksson2@regionorebrolan.se>
2026-02-03 14:44:27 +0100

    Base class from db models

---
+++ client/RolDbContext/EntryBase.cs
@@ -0,0 +1,10 @@
+﻿namespace RolDbContext
+{
+    public class EntryBase
+    {
+        public Guid Id { get; set; }
+        public int Version { get; set; }
+        public string? Status { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
--- client/RolDbContext/Info.cs
+++ client/RolDbContext/Info.cs
@@ -1,8 +1,7 @@
 ﻿namespace RolDbContext
 {
-    public class Info
+    public class Info : EntryBase
     {
-        public Guid Id { get; set; }
         public string Title { get; set; }
         public string Text { get; set; }
         public DateTime PublishDate { get; set; }
