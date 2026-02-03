dd753d46965cc173e2acee678dc1932e59d57c6f
Tobbe Eriksson <torbjorn.eriksson2@regionorebrolan.se>
2026-02-03 16:53:55 +0100

    Start with mappers

---
+++ client/clientside.backend/clientside.backend/Mappers/Mappers.cs
@@ -0,0 +1,15 @@
+﻿namespace clientside.backend.Mappers
+{
+    public static class Mappers
+    {
+        public static viewmodels.Info? Map(this RolDbContext.Models.Info? info)
+         {
+            if (info == null) return null;
+            var response = new viewmodels.Info
+            {
+
+            };
+            return response;
+        }
+    }
+}
